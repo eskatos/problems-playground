edc15c322a46ae2f6facd682c58168d565d0978f
Donát Csikós <csdonat@gmail.com>
2025-12-03 12:01:46 +0100

    Plugin and deprecation scenarios

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,8 @@ problems.reporter.report(
     contextualLabel("This is not a dependency")
 }
 
+//// Compilation
+
 // Java compilation - warning related to code being compiled
 
 // Java compilation - error related to code being compiled
@@ -37,4 +39,20 @@ problems.reporter.report(
 
 // Kotlin compilation - compiler cannot be invoked (binary not found, process segfaults,etc.)
 
+
+//// Plugins
+
+// Shadow plugin - cannot merge services files
+
+// Version plugin (com.github.ben-manes.versions) unsupported/invalid version formats
+
+//// Deprecation
+
+// Gradle invoked with deprecated feature enabled
+
+// build logic (build script, buildSrc, build-logic, local plugin, binary plugin) uses deprecated Gradle API
+
+// build logic (build script, buildSrc, build-logic, local plugin, binary plugin) uses deprecated plugin API
+
+// build logic (build script, buildSrc, build-logic, local plugin, binary plugin) uses plugin API that uses deprecated Gradle API
 
